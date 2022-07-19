ae12653308fe0dd683698963e41ca1da162085a6
Beatzoid <43425381+Beatzoid@users.noreply.github.com>
2022-07-19 09:53:43 -0700

    ✨ Tabs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 
-const routes: Routes = [];
+import { BookmarksComponent } from "./bookmarks/bookmarks.component";
+import { NotesComponent } from "./notes/notes.component";
+import { TodosComponent } from "./todos/todos.component";
+
+const routes: Routes = [
+    { path: "bookmarks", component: BookmarksComponent },
+    { path: "todos", component: TodosComponent },
+    { path: "notes", component: NotesComponent },
+];
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
